0ca393089c4c9b642acc9a14380d82b3e7f20ec8
DouiabAsmaa <asmaa.douiab@gmail.com>
2018-05-19 17:39:34 +0200

    Insertion de Template Admin

---
+++ EcommerceAdmin/App_Start/BundleConfig.cs
@@ -0,0 +1,38 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace EcommerceAdmin
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+           /* bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));*/
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+           /* bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));*/
+
+            bundles.Add(new ScriptBundle("~/Js").Include(
+                      "~/Content/plugins/jQuery/jquery-2.2.3.min.js",
+                      "~/Content/bootstrap/js/bootstrap.min.js",
+                      "~/Content/plugins/slimScroll/jquery.slimscroll.min.js",
+                      "~/Content/plugins/fastclick/fastclick.js",
+                       "~/Content/dist/js/app.min.js",
+                        "~/Content/dist/js/demo.js"
+                      ));
+
+            bundles.Add(new StyleBundle("~/Css").Include(
+                      "~/Content/bootstrap/css/bootstrap.min.css",
+                      "~/Content/dist/css/AdminLTE.min.css",
+                      "~/Content/dist/css/skins/_all-skins.min.css"));
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
