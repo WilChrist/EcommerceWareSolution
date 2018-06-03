a1a915fc8c34ac241446006d5ef71438a5ca2d17
francisDYL <leroifrancis56@gmail.com>
2018-06-03 17:15:43 +0000

    correction bug

--- EcommerceWare.API/Controllers/CustomersController.cs
+++ EcommerceWare.API/Controllers/CustomersController.cs
@@ -32,7 +32,26 @@ namespace EcommerceWare.API.Controllers
         }
 
         // GET: api/Customers/5
-        public CustomersForView Post()
+        public CustomersForView Post([FromBody]Login_informationsForView li_u)
+        {
+            ecommerceEntities db = new ecommerceEntities();
+
+            //   db.InsertCustomer("francis", "123", "Le Roi", "King", "fr@fr.com", "696598600", "jk", "io", "11-400", "kl", 15, "op");
+
+            Login_informations li = db.Login_informations.SingleOrDefault(l => l.Login == li_u.Login && l.Password == li_u.Password);
+            CustomersForView user;
+            if (li != null)
+            {
+                user = new CustomersForView(db.Customers.SingleOrDefault(c => c.IdLogin_information == li.IdLogin_information));
+                return user;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+       /* public CustomersForView Post(String login, String password)
         {
             ecommerceEntities db = new ecommerceEntities();
 
@@ -50,12 +69,11 @@ namespace EcommerceWare.API.Controllers
                 return null;
             }
         }
-
         // POST: api/Customers
         public void Post([FromBody]string value)
         {
         }
-
+        */
         // PUT: api/Customers/5
         public void Put(int id, [FromBody]string value)
         {
