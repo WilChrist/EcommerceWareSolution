4329a5b83670c169fd357e8f0ecfa52272cafdeb
francisDYL <leroifrancis56@gmail.com>
2018-06-08 23:28:56 +0000

    fin provispore

--- EcommerceWare.API/Controllers/CustomersController.cs
+++ EcommerceWare.API/Controllers/CustomersController.cs
@@ -51,32 +51,20 @@ namespace EcommerceWare.API.Controllers
             }
         }
 
-       /* public CustomersForView Post(String login, String password)
+        /* public Boolean  Post(String login,String password,String email, String last_name)
+         {
+             /*ecommerceEntities db = new ecommerceEntities();
+             db.InsertCustomer(login, password, last_name, last_name, email, "696598600", "jk", "io", "11-400", "kl", 15, "op");
+            return true;
+             
+
+         }*/
+
+        public Boolean PutAddCustomer(SignUpData data)
         {
             ecommerceEntities db = new ecommerceEntities();
-
-            //   db.InsertCustomer("francis", "123", "Le Roi", "King", "fr@fr.com", "696598600", "jk", "io", "11-400", "kl", 15, "op");
-
-            Login_informations li = db.Login_informations.SingleOrDefault(l => l.Login == login && l.Password == password);
-            CustomersForView user;
-            if (li != null)
-            {
-                user = new CustomersForView(db.Customers.SingleOrDefault(c => c.IdLogin_information == li.IdLogin_information));
-                return user;
-            }
-            else
-            {
-                return null;
-            }
-        }
-        // POST: api/Customers
-        public void Post([FromBody]string value)
-        {
-        }
-        */
-        // PUT: api/Customers/5
-        public void Put(int id, [FromBody]string value)
-        {
+            db.InsertCustomer(data.Login, data.Password, data.Last_name, data.Last_name, data.Email, "696598600", "jk", "io", "11-400", "kl", 15, "op");
+            return true;
         }
 
         // DELETE: api/Customers/5
---
+++ EcommerceWare.API/Models/SignUpData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceWare.API.Models
+{
+    public class SignUpData
+    {
+        public String Login { get; set; }
+        public String Password { get; set; }
+        public String Email { get; set; }
+        public String Last_name { get; set; }
+    }
+}
